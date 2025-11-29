2e48c613fea70d794f28c8c4c70fb8dfa51c0dc8
Pehesara Munasinghe <pehesaramunassinghe13@gmail.com>
2025-11-29 18:26:19 +0530

    Updated and Cloned

---
+++ src/main/java/com/pharmacy/medlan/MedlanApplication.java
@@ -0,0 +1,13 @@
+package com.pharmacy.medlan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedlanApplication.class, args);
+	}
+
+}
